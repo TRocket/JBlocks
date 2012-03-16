0dd78b04ba1d99d08cf1b879405665d8a6840981
TRocket <TRocket@itchypp.github>
2012-03-16 19:17:34 +0000

    git-svn-id: http://svn.code.sf.net/p/jblocks/code-0/trunk@5 9daefd43-015a-43da-9b33-cb4dc19dcf47

---
+++ org/jblocks/JBlocks.java
@@ -0,0 +1,19 @@
+package org.jblocks;
+
+/**
+ * 
+ * @author TRocket
+ *
+ */
+public class JBlocks {
+
+	/**
+	 * this is the standalone main method
+	 * @param args the command line args
+	 */
+	public static void main(String[] args) {
+		// TODO run JBlocks
+
+	}
+
+}
