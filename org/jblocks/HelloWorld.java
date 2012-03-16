1f8c6882b0a4e8300364cc69c7da05749d5c5123
zeroluck <zeroluckbgn@gmail.com>
2012-03-16 20:27:17 +0000

    ZeroLuck's HelloWorld test!
    git-svn-id: http://svn.code.sf.net/p/jblocks/code-0/trunk@6 9daefd43-015a-43da-9b33-cb4dc19dcf47

---
+++ org/jblocks/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jblocks;
+
+/**
+ *
+ * @author ZeroLuck
+ */
+public class HelloWorld {
+
+
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World from ZeroLuck!");
+        System.out.println("I am happy! It works! :D");
+    }
+}
