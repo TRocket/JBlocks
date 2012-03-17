6d9a45c309863fb5da53fe196451d4bf3a0db0cc
TRocket <TRocket@itchypp.github>
2012-03-17 08:24:59 +0000

    JBlocksLauncher contains the main method for standalone use
    git-svn-id: http://svn.code.sf.net/p/jblocks/code-0/trunk@9 9daefd43-015a-43da-9b33-cb4dc19dcf47

--- org/jblocks/JBlocks.java
+++ org/jblocks/JBlocks.java
@@ -1,19 +1,5 @@
 package org.jblocks;
 
-/**
- * 
- * @author TRocket
- * @author ZeroLuck <-- :D
- */
 public class JBlocks {
-
-	/**
-	 * this is the standalone main method
-	 * @param args the command line args
-	 */
-	public static void main(String[] args) {
-		// TODO run JBlocks
-
-	}
 
 }
---
+++ org/jblocks/JBlocksLauncher.java
@@ -0,0 +1,19 @@
+package org.jblocks;
+
+/**
+ * 
+ * @author TRocket
+ * @author ZeroLuck <-- :D
+ */
+public class JBlocksLauncher {
+
+	/**
+	 * this is the standalone main method
+	 * @param args the command line args
+	 */
+	public static void main(String[] args) {
+		// TODO run JBlocks
+
+	}
+
+}
