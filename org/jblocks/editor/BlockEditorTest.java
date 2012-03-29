a2e7ab5f7226307e6944857f1302ceae24ccb528
zeroluck <zeroluckbgn@gmail.com>
2012-03-29 17:50:20 +0000

    Changes: - JBlockEditor update. (has glitchs)
    ZeroLuck
    
    git-svn-id: http://svn.code.sf.net/p/jblocks/code-0/trunk@55 9daefd43-015a-43da-9b33-cb4dc19dcf47

--- org/jblocks/editor/BlockEditorTest.java
+++ org/jblocks/editor/BlockEditorTest.java
@@ -3,6 +3,7 @@ package org.jblocks.editor;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 /**
  *
@@ -10,18 +11,23 @@ import javax.swing.JFrame;
  */
 class BlockEditorTest {
     
-    public static void main(String[] args) {    
+    public static void main(String[] args) {        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Throwable t) {
+            t.printStackTrace(System.err);
+        }
         JFrame frm = new JFrame("Block-Editor : Test");
         frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frm.setLocationByPlatform(true);
-
+        
         frm.setLayout(new BorderLayout());
         JBlockEditor ch = new JBlockEditor();
         
-        ch.addCategory("Motion", Color.BLUE); 
+        ch.addCategory("Control", Color.ORANGE);
+        ch.addCategory("Motion", Color.BLUE);        
         ch.addCategory("Operators", Color.GREEN);
         ch.addCategory("Lists", Color.RED);
-        ch.addCategory("Control", Color.ORANGE);
         
         ch.addCategory("Touching", Color.CYAN);
         ch.addCategory("Pen", Color.GREEN.darker());
@@ -31,15 +37,18 @@ class BlockEditorTest {
         JScriptPane pane = new JScriptPane();
         pane.add(JScriptPane.createBlock("hat", "When %{gf} clicked"));
         pane.cleanup();
-                
+        
         ch.setScriptPane(pane);
+        
+        ch.addBlock("Control", JScriptPane.createBlock("hat", "When %{gf} clicked"));
+        ch.addBlock("Control", JScriptPane.createBlock("hat", "When key %{combo;space;a;b;c;d;e;f} pressed"));
         
         
         frm.add(ch);
         frm.pack();
-
+        
         
         frm.setVisible(true);
-   //     pane.cleanup();
+        
     }
 }
--- org/jblocks/editor/JBlockEditor.java
+++ org/jblocks/editor/JBlockEditor.java
@@ -2,7 +2,17 @@ package org.jblocks.editor;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import org.jblocks.JBlocks;
 
 /**
  *
@@ -15,9 +25,23 @@ import javax.swing.JPanel;
  */
 public class JBlockEditor extends JPanel {
 
+    // <global>
+    private static BufferedImage scriptpane;
+
+    static {
+        try {
+            scriptpane = ImageIO.read(JBlocks.class.getResourceAsStream("res/blockchooser.png"));
+        } catch (IOException ex) {
+            throw new java.lang.ExceptionInInitializerError(ex);
+        }
+    }
+    // <member>
+    private JPanel ctgPanel;
     private JScriptPane pane;
+    private JScriptPane ctgScriptPane;
     private JCategoryChooser chooser;
-    
+    private HashMap<String, JScriptPane> ctgs = new HashMap<String, JScriptPane>();
+
     /**
      * creates a block-editor with an <b>empty</b> category-chooser. <br />
      * 
@@ -26,11 +50,15 @@ public class JBlockEditor extends JPanel {
      */
     public JBlockEditor() {
         chooser = new JCategoryChooser();
-        
+        ctgPanel = new JPanel();
+        ctgPanel.setLayout(new BorderLayout());
+        ctgPanel.setBorder(BorderFactory.createBevelBorder(4, Color.yellow, Color.black));
+
         setLayout(new BorderLayout());
-        add(chooser, BorderLayout.WEST);
+        add(ctgPanel, BorderLayout.WEST);
+        ctgPanel.add(chooser, BorderLayout.NORTH);
     }
-    
+
     /**
      * switches between script-panes. <br />
      * 
@@ -47,6 +75,20 @@ public class JBlockEditor extends JPanel {
         }
     }
 
+    public void switchCategory(String name) {
+        System.out.println(name);
+        JScriptPane bck = ctgScriptPane;
+        ctgScriptPane = ctgs.get(name);
+        if (ctgScriptPane != null) {
+            if (bck != null) {
+                ctgPanel.remove(bck.getParent());
+            }
+            ctgPanel.add(new JScrollPane(ctgScriptPane), BorderLayout.CENTER);
+            ctgPanel.validate();
+        }
+    }
+    private boolean firstCtg = true;
+
     /**
      * Adds a new category to the block-chooser. <br />
      * 
@@ -54,10 +96,43 @@ public class JBlockEditor extends JPanel {
      * @param name - the name of the category.
      * @param c - the color of the category.
      */
-    public void addCategory(String name, Color c) {
-        chooser.addCategory(name, c);
+    public void addCategory(final String name, Color c) {
+        if (ctgs.containsKey(name)) {
+            return;
+        }
+        JComponent comp = chooser.addCategory(name, c);
+        comp.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                switchCategory(name);
+            }
+        });
+        JScriptPane p = new JScriptPane();
+        p.setScriptPaneImage(scriptpane);
+        ctgs.put(name, p);
+
+        if (firstCtg) {
+            switchCategory(name);
+            firstCtg = false;
+        }
     }
-    
+
+    /**
+     * Adds a new block to a category. <br />
+     * 
+     * 
+     * @param name - the name of the category
+     * @param b - the block
+     */
+    public void addBlock(String name, AbstrBlock b) {
+        JScriptPane p = ctgs.get(name);
+        if (p != null) {
+            p.add(b);
+            p.cleanup();
+        }
+    }
+
     /**
      * 
      * @return - the current script-pane.
